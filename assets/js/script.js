b549bb4ab9d7b52ccf440ebd15e1d7b50fec958a
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-05-21 20:50:23 -0400

    added psuedocode for javascript functionality

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//timer (60s timer)
+//start button (starts quiz)
+//question list (multiple choice and short response)
+//answer list (multiple choice and short response)
+//show right/wrong (wrong subtracts 5s)
+//game over (all questions answered or time ran out)
+//save high scores in local storage with initials
