60b7919186b2cfbda5455db7ef20e2578ec2d9c8
codeBiscuit <josh.rinehart.2k@gmail.com>
2022-05-21 21:13:16 -0400

    added start button functionality and created a 60 second timer for the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
-//timer (60s timer)
-//start button (starts quiz)
+var timerEl = document.querySelector(".countdown-num");
+var startButton = document.querySelector(".start-button");
+var question = document.querySelector(".question-box");
+
+var questionList = ["Inside which HTML element do we put the Javascript?", ""]
+
+var answerList = ["<script>", ]
+
+function timer(){
+    var timeLeft = 60;
+    
+    var timeInterval = setInterval(function(){
+        if(timeLeft > 1){
+            timeLeft--;
+            timerEl.textContent = timeLeft;
+            console.log(timeLeft)
+        }
+        else{
+            clearInterval(timeInterval);
+            timerEl.textContent = "GAME OVER";
+        }
+    }, 1000);
+}
+
+startButton.addEventListener("click", timer);
+
+//timer (60s timer) DONE
+//start button (starts quiz) DONE
 //question list (multiple choice and short response)
 //answer list (multiple choice and short response)
 //show right/wrong (wrong subtracts 5s)
